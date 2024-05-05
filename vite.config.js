9939e0adad42d4bd30fda33e9d1242775bce0ff3
suprefuner <suprefuner@gmail.com>
2024-05-05 20:42:28 +0800

    test netlify env var

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import netlifyEdge from "@netlify/vite-plugin-netlify-edge"
-i
 
 // https://vitejs.dev/config/
 export default defineConfig({
